9bf7599664d6caeb575620e7d83e06e8c7d20eb8
Kenny A. Francia Augusto <KFrancia@onpe.gob.pe>
2023-05-21 09:06:39 -0500

    buscar caterogia por id

---
+++ src/main/java/com/company/inventory/response/CategoryResponseRest.java
@@ -0,0 +1,12 @@
+package com.company.inventory.response;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class CategoryResponseRest extends ResponseRest{
+	
+	private CategoryResponse categoryResponse = new CategoryResponse();
+
+}
